be951247e00cd37c194997b992ad3f366eb634b5
egornovivan <60801856+egornovivan@users.noreply.github.com>
2021-07-22 02:23:50 +0500

    Dirtfix for et tu
    https://nma-fallout.com/threads/hq-music-for-fallout-2.220421/#post-4435384

--- extra/publish.sh
+++ extra/publish.sh
@@ -13,6 +13,9 @@ for q in lq_ebur128 hq_ebur128 uhq_ebur128; do
   fname="fallout_1_2_${q}_music_${version}.zip"
   cp "LICENSE.md" "$q"
   cd "$q"
+  for f in 03wrldmp.*; do 
+    cp -- "$f" "03world${f#03wrldmp}"
+  done
   zip "$fname" *
   mv "$fname" ..
   cd ..
